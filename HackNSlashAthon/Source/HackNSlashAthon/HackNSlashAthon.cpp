8e88ed4bed0b809d7066459a54848cb5607bdb9f
SalvaGM <salvagarciamendez@gmail.com>
2026-02-06 21:14:48 +0000

    Update: Readded all items following gitignore

---
+++ HackNSlashAthon/Source/HackNSlashAthon/HackNSlashAthon.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HackNSlashAthon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HackNSlashAthon, "HackNSlashAthon" );
+
+DEFINE_LOG_CATEGORY(LogHackNSlashAthon)
+ 
---
+++ README.md
@@ -0,0 +1,2 @@
+# HackNSlashAthon
+Supercell Hackathon Feb 2026 - Hack N Slack game using GenAI made by @salvagm &amp; @danielcabero8
